24ec75d8650b51687fb3f9ba66585db14edf1a0f
Stefan D. <demirel.stefan@yahoo.com>
2024-10-11 14:51:16 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-[100vh] flex justify-center items-center'>Title</div>
+    </>
+  )
+}
+
+export default App
